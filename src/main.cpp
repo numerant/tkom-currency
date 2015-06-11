776633b8a087698270756f2ff7f3c592ec00fdcf
Jakub Maleszewski <numerant@gmail.com>
2015-06-11 09:57:54 +0200

    main.cpp changes

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,11 @@
 using namespace std;
 using namespace parser;
 
-/*
- *
- */
 int main(int argc, char** argv)
 {
-//    Scanner scanner(cin);
-//
-//    while(cin)
-//    {
-//        scanner.readNextToken();
-//        cout << scanner.getToken().toString() << "\n";
-//    }
-
-      Parser p(std::make_unique<Scanner>(std::cin));
-      auto var1 = std::move(p.parse());
-      var1->execute();
+    Parser p(std::make_unique<Scanner>(std::cin));
+    auto var1 = std::move(p.parse());
+    var1->execute();
 
     return 0;
 }
